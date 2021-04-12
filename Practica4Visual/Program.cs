b4bf701541f9a5fb3dcd48f1e2649b097a201e21
Alvaro Lozano <alvarloz@ucm.es>
2021-04-12 11:52:30 +0200

    Archivos iniciales añadidos al índice del repositorio

---
+++ Practica4Visual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica4Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
